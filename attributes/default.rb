199ac12a8d25d364812a02853b8d19d17c4e7ffd
Chris Roberts <chrisroberts.code@gmail.com>
2012-09-18 15:42:58 -0700

    Add relocation message

--- attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default[:red_unicorn][:version] = '> 0'
-default[:red_unicorn][:exec] = File.join(node[:languages][:ruby][:bin_dir], 'red_unicorn')
--- recipes/default.rb
+++
@@ -1,6 +0,0 @@
-
-include_recipe 'bluepill'
-
-gem_package 'red_unicorn' do
-  version node[:red_unicorn][:version]
-end
